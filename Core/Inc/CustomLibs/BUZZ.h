e006649f9e460bb2a1f30553a0511efdd1d3e0e2
pablonhernandez <pablo.nhernandez@alumnos.upm.es>
2024-01-01 14:05:36 +0100

    BUZZER
    Activacion de buzzer despues de x tiempo hecho con output compare en modo set
    
    Co-Authored-By: sdevargas <47787286+sdevargas@users.noreply.github.com>

---
+++ Core/Inc/CustomLibs/BUZZ.h
@@ -0,0 +1,5 @@
+#include "stm32f4xx.h"
+#include "stm32f4xx_hal.h"
+
+//TIM3 CHANNEL_2 -> OUTPUT COMPARE SET ON MATCH; ¿1 MIN?
+void BUZZ_Init(TIM_HandleTypeDef* htim);
---
+++ Core/Src/CustomLibs/BUZZ.c
@@ -0,0 +1,9 @@
+#include "stm32f4xx.h"
+#include "stm32f4xx_hal.h"
+#include "BUZZ.h"
+
+void BUZZ_Init(TIM_HandleTypeDef* htim){
+  	HAL_TIM_PWM_Start(htim, TIM_CHANNEL_2);
+
+  	__HAL_TIM_SET_COMPARE(htim, TIM_CHANNEL_1, 60000);
+  }
