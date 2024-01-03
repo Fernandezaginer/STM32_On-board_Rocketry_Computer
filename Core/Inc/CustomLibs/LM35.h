50b60ad8e77b7fade7b9a90b96b5c68961c3c335
pablonhernandez <pablo.nhernandez@alumnos.upm.es>
2024-01-03 17:01:13 +0100

    Sensor de temperatura LM35 y arreglo buzzer
    LM35 por ADC.
    Arreglo buzzer
    
    Co-Authored-By: sdevargas <47787286+sdevargas@users.noreply.github.com>

---
+++ Core/Inc/CustomLibs/LM35.h
@@ -0,0 +1,5 @@
+#include "stm32f4xx.h"
+#include "stm32f4xx_hal.h"
+
+
+float LM_Temp_Read(ADC_HandleTypeDef* hadc);
--- Core/Src/CustomLibs/BUZZ.c
+++ Core/Src/CustomLibs/BUZZ.c
@@ -3,7 +3,7 @@
 #include "BUZZ.h"
 
 void BUZZ_Init(TIM_HandleTypeDef* htim){
-  	HAL_TIM_PWM_Start(htim, TIM_CHANNEL_2);
+  	HAL_TIM_OC_Start(htim, TIM_CHANNEL_2);
 
   	__HAL_TIM_SET_COMPARE(htim, TIM_CHANNEL_1, 60000);
   }
